99c5fa4aef8acaeec4bcc45cd1e87d9854cf5163
sandeshlawhale <sandeshlawhale@gmail.com>
2025-04-21 15:39:11 +0530

    added proxy to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
+  proxy: {
+    "/api": {
+      target: "https://trueno-backend.vercel.app", // Replace with your actual backend URL
+      changeOrigin: true, // Needed for virtual hosted sites
+      secure: false, // Set this to false if using http for local testing
+      // rewrite: (path) => path.replace(/^\/api/, ""), // Removes the /api prefix when proxying
+    },
   },
 });
